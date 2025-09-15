bce9f6874c75ebb625211cf65b05eda88e714705
NND <nedelkonikita345@gmail.com>
2025-09-15 12:20:03 +0300

    feat: добавлены иконки svg

---
+++ frontend/src/components/ui/icons.tsx
@@ -0,0 +1,22 @@
+// src/components/ui/icons.tsx
+import {
+    Monitor,
+    Projector,
+    Search,
+    Funnel,
+    ChevronDown,
+    MapPin,
+    Clock,
+    CalendarDays,
+} from "lucide-react";
+
+export const Icons = {
+    Monitor,
+    Projector,
+    Search,
+    Funnel,
+    ChevronDown,
+    MapPin,
+    Clock,
+    CalendarDays,
+};
