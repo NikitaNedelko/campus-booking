f855ba9089cc48a7607d947c1662aa4b6de0c6e5
NND <nedelkonikita345@gmail.com>
2025-11-28 18:30:25 +0300

    chore(db): сделал базовую модель для Alembic

---
+++ backend/app/db/models/__init__.py
@@ -0,0 +1,7 @@
+from app.db.base_model import Base
+
+# Import SQLAlchemy models here so Alembic sees metadata during autogenerate.
+# Example:
+# from app.db.models.user import User  # noqa: F401
+
+__all__ = ["Base"]
